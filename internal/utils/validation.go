8cee88f5ee09857e0be10e9fd5c3feb5a84e54ba
Ng1n3 <code.with.muyiwa@gmail.com>
2025-09-22 04:14:15 +0000

    chore: proper input validation in validation.go

---
+++ internal/utils/validation.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"strings"
+	"time"
+
+	"github.com/Ng1n3/go-todo/internal/errors"
+)
+
+func ValidateTask (task string) (string, error) {
+  task = strings.TrimSpace(task)
+  if len(task) < 2 {
+    return "", errors.ErrTaskTooShort
+  }
+
+  return task, nil
+}
+
+func ValidateDate(dateStr string) (time.Time, error) {
+  dateStr = strings.TrimSpace(dateStr)
+  if dateStr == "" {
+    return time.Time{}, errors.ErrInvalidDateFormat
+  }
+
+  parsedDate, err := time.Parse("2006-01-02", dateStr)
+  if err != nil {
+    return time.Time{}, errors.ErrInvalidDateFormat
+  }
+
+  return parsedDate, nil
+}
+
+func ValidateLabels(labelsInput string) []string {
+  if labelsInput == "" {
+    return []string{}
+  }
+
+  labelsInput = strings.TrimSpace(labelsInput)
+  labels := strings.Split(labelsInput, ",")
+
+  var cleanLabel []string
+  for _, label := range labels {
+    label = strings.TrimSpace(label)
+    if label != "" {
+      cleanLabel = append(cleanLabel, label)
+    }
+  }
+
+  return cleanLabel
+}
