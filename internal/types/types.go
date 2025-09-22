b9fa5f5b0a6421b0667d2b42057983dd0170d413
Ng1n3 <code.with.muyiwa@gmail.com>
2025-09-22 03:45:57 +0000

    chore: proper validation

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,6 +1,9 @@
 package types
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Priority string
 
@@ -11,12 +14,37 @@ const (
 )
 
 type Todo struct {
-	ID        string
-	Task      string
-	Labels    []string
-	Completed bool
-	DueDate   time.Time
-	Priority  Priority
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `json:"id"`
+	Task      string    `json:"task"`
+	Labels    []string  `json:"labels"`
+	Completed bool      `json:"completed"`
+	DueDate   time.Time `json:"due_date"`
+	Priority  Priority  `json:"priority"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (p Priority) Validate() error {
+	switch p {
+	case High, Medium, Low:
+		return nil
+	default:
+		return fmt.Errorf("\ninvalid Priority: %s. Must be HIGHT, MEDIUM, OR LOW", p)
+	}
+}
+
+func (t *Todo) Validate() error {
+	if len(t.Task) < 2 {
+		return fmt.Errorf("\ntask must be at least 2 characters long\n")
+	}
+
+	if t.ID == "" {
+		return fmt.Errorf("\ntodo ID cannot be empty\n")
+	}
+
+	if err := t.Priority.Validate(); err != nil {
+		return err
+	}
+
+	return nil
 }
